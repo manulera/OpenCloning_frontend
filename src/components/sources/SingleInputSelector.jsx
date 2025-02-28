804b49e6661a048ea63942d7000c79cad1a52ff4
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-02-28 12:09:38 +0000

    closes #400

--- src/components/sources/SingleInputSelector.jsx
+++ src/components/sources/SingleInputSelector.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
 import { FormHelperText, InputLabel, MenuItem, Select } from '@mui/material';
+import { isEqual } from 'lodash-es';
 import { getIdsOfEntitiesWithoutChildSource } from '../../store/cloning_utils';
 
 function SingleInputSelector({ selectedId, onChange, label, inputEntityIds, allowUnset = false, helperText = '', disabled = false }) {
   const idsWithoutChild = useSelector(({ cloning }) => getIdsOfEntitiesWithoutChildSource(cloning.sources, cloning.entities), shallowEqual);
   const options = [...new Set([...idsWithoutChild, ...inputEntityIds])];
-  const renderedOptions = options.sort().map((id) => <MenuItem key={id} value={id}>{id}</MenuItem>);
+  const sequenceNames = useSelector(({ cloning }) => options.map((id) => ({ id, name: cloning.teselaJsonCache[id].name })), isEqual);
+  const renderedOptions = options.sort().map((id) => (
+    <MenuItem key={id} value={id}>
+      {`${id} - ${sequenceNames.find(({ id: id2 }) => id2 === id).name}`}
+    </MenuItem>
+  ));
   if (allowUnset) {
     renderedOptions.unshift(<MenuItem key="unset" value=""><em>None</em></MenuItem>);
   }
