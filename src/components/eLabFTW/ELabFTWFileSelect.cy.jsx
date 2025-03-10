0a409fab829fe83cf03b8480949c2faddde385ee
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-03-10 18:56:46 +0000

    first component test

---
+++ src/components/eLabFTW/ELabFTWFileSelect.cy.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ELabFTWFileSelect from './ELabFTWFileSelect';
+import { eLabFTWHttpClient, readHeaders } from './common';
+
+// Stub readHeaders
+beforeEach(() => {
+  cy.stub(readHeaders, 'Authorization').value('test');
+});
+
+describe('<ELabFTWFileSelect />', () => {
+  it('shows the right options', () => {
+    const setFileInfoSpy = cy.spy().as('setFileInfoSpy');
+    // Stub the eLabFTWHttpClient similar to the pattern in eLabFTWInterface.test.js
+    cy.stub(eLabFTWHttpClient, 'get').withArgs('/api/v2/items/1', { headers: { Authorization: 'test' } }).resolves({
+      data: {
+        uploads: [
+          { id: 1, real_name: 'file1.txt' },
+          { id: 2, real_name: 'file2.txt' },
+        ],
+      },
+    });
+    cy.mount(<ELabFTWFileSelect fullWidth itemId={1} setFileInfo={setFileInfoSpy} />);
+    cy.get('div.MuiSelect-select').click();
+    cy.get('li').contains('file1.txt').should('exist');
+    cy.get('li').contains('file2.txt').should('exist');
+    cy.get('li').contains('file1.txt').click();
+    cy.get('@setFileInfoSpy').should('have.been.calledWith', { id: 1, real_name: 'file1.txt' });
+  });
+  it('shows empty options if no files are found', () => {
+    cy.mount(<ELabFTWFileSelect fullWidth itemId={1} />);
+    cy.stub(eLabFTWHttpClient, 'get').withArgs('/api/v2/items/1', { headers: { Authorization: 'test' } }).resolves({
+      data: {
+        uploads: [],
+      },
+    });
+    cy.get('div.MuiSelect-select').click();
+    cy.get('li').should('not.exist');
+  });
+
+  it('shows an error message if the request fails and can retry', () => {
+    cy.mount(<ELabFTWFileSelect fullWidth itemId={1} />);
+    cy.get('.MuiAlert-message').should('contain', 'Could not retrieve attachment');
+    // Clicking the retry button makes the request again
+    cy.spy(eLabFTWHttpClient, 'get').as('eLabFTWHttpClientSpy');
+    cy.get('button').contains('Retry').click();
+    cy.get('@eLabFTWHttpClientSpy').should('have.been.calledWith', '/api/v2/items/1', { headers: { Authorization: 'test' } });
+  });
+});
