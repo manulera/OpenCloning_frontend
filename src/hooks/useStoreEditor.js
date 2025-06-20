0a00ad0284d6f3fa7ea13ae48c9a5f6eea26a05d
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-06-20 18:31:41 +0100

    use file name of sequencing files as sequence names alignment editor

--- src/hooks/useStoreEditor.js
+++ src/hooks/useStoreEditor.js
@@ -73,7 +73,7 @@ export default function useStoreEditor() {
                 }
               }
               return {
-                sequenceData: alignmentSequenceData,
+                sequenceData: {...alignmentSequenceData, name: aln.file_name},
                 alignmentData: {
                   sequence: aln.alignment[1],
                 },
