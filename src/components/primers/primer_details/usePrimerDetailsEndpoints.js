aecb8fc0b16b00f0faf2628ba55f294f99cc4d74
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-10-14 17:01:47 +0100

    WIP with tests

--- src/components/primers/primer_details/usePrimerDetailsEndpoints.js
+++ src/components/primers/primer_details/usePrimerDetailsEndpoints.js
@@ -6,35 +6,43 @@ import { useSelector } from 'react-redux';
 
 const primerDetailsCache = new Map();
 const heterodimerDetailsCache = new Map();
+export function makePrimerDetailsCacheKey(sequence, globalPrimerSettings) {
+  const settingsKey = JSON.stringify(globalPrimerSettings);
+  return `${String(sequence)}::${settingsKey}`;
+}
+export function makeHeterodimerCacheKey(sequence1, sequence2, globalPrimerSettings) {
+  const pairKey = [sequence1, sequence2].map(String).sort().join(',');
+  const settingsKey = JSON.stringify(globalPrimerSettings);
+  return `${pairKey}::${settingsKey}`;
+}
 export function usePrimerDetailsEndpoints() {
   const backendRoute = useBackendRoute();
   const httpClient = useHttpClient();
   const globalPrimerSettings = useSelector((state) => state.cloning.globalPrimerSettings, isEqual);
 
-  React.useEffect(() => {
-    primerDetailsCache.clear();
-    heterodimerDetailsCache.clear();
-  }, [globalPrimerSettings]);
+  // Caches are keyed by both the input sequence(s) and the current globalPrimerSettings,
+  // so we don't need to clear them on settings change.
 
   const url = backendRoute('primer_details');
   const heterodimerUrl = backendRoute('primer_heterodimer');
 
   const getPrimerDetails = useCallback(async (sequence) => {
-    if (!primerDetailsCache.has(sequence)) {
+    const cacheKey = makePrimerDetailsCacheKey(sequence, globalPrimerSettings);
+    if (!primerDetailsCache.has(cacheKey)) {
       const { data } = await httpClient.post(url, { sequence, settings: globalPrimerSettings });
       data.length = sequence.length;
-      primerDetailsCache.set(sequence, data);
+      primerDetailsCache.set(cacheKey, data);
     }
-    return primerDetailsCache.get(sequence);
+    return primerDetailsCache.get(cacheKey);
   }, [url, httpClient, globalPrimerSettings]);
 
   const getHeterodimerDetails = useCallback(async (sequence1, sequence2) => {
-    const key = [sequence1, sequence2].sort().join(',');
-    if (!heterodimerDetailsCache.has(key)) {
+    const cacheKey = makeHeterodimerCacheKey(sequence1, sequence2, globalPrimerSettings);
+    if (!heterodimerDetailsCache.has(cacheKey)) {
       const { data } = await httpClient.post(heterodimerUrl, { sequence1, sequence2, settings: globalPrimerSettings });
-      heterodimerDetailsCache.set(key, data);
+      heterodimerDetailsCache.set(cacheKey, data);
     }
-    return heterodimerDetailsCache.get(key);
+    return heterodimerDetailsCache.get(cacheKey);
   }, [heterodimerUrl, httpClient, globalPrimerSettings]);
 
   return { getPrimerDetails, getHeterodimerDetails };
