9222862b1d55c04d9882794aa7a19c1a413ee10f
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-10-22 16:23:22 +0100

    fix eslint

---
+++ eslint.config.js
@@ -0,0 +1,65 @@
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import cypress from 'eslint-plugin-cypress';
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'module',
+            globals: {
+                browser: true,
+                __APP_VERSION__: 'readonly',
+                window: 'readonly',
+                document: 'readonly',
+                console: 'readonly',
+                process: 'readonly',
+                Buffer: 'readonly',
+                global: 'readonly',
+                module: 'readonly',
+                require: 'readonly',
+                exports: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly'
+            },
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
+        },
+        plugins: {
+            react,
+            'react-hooks': reactHooks
+        },
+        rules: {
+            ...react.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+            'max-len': ['error', { code: 300 }],
+            'react/prop-types': 0,
+            'object-curly-newline': 0,
+            'import/prefer-default-export': 0,
+            'indent': ['error', 2],
+            'no-unused-vars': 'warn',
+            'no-console': 'warn'
+        },
+        settings: {
+            react: {
+                version: 'detect'
+            }
+        }
+    },
+    {
+        files: ['cypress/**/*.{js,jsx}'],
+        plugins: {
+            cypress
+        },
+        rules: {
+            ...cypress.configs.recommended.rules
+        }
+    }
+];
