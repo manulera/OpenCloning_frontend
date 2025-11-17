09641cdbac2eab209d271a67f5db0ac79f0378e3
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-11-17 23:58:16 +0000

    update backend and fix add primer button from CRISPR

--- src/components/primers/SelectPrimerForm.jsx
+++ src/components/primers/SelectPrimerForm.jsx
@@ -13,6 +13,8 @@ function chipRenderer(selected, primers) {
 }
 
 function SelectPrimerForm({ primers, selected, onChange, goToPrimerTab, label, multiple = false }) {
+  const [open, setOpen] = React.useState(false);
+
   React.useEffect(() => {
     // If the primer that was selected is deleted
     const primerIds = primers.map(({ id }) => id);
@@ -24,17 +26,34 @@ function SelectPrimerForm({ primers, selected, onChange, goToPrimerTab, label, m
       onChange(selected.filter((id) => primerIds.includes(id)));
     }
   }, [primers]);
+
+  const onChangeHandleCreatePrimer = (value) => {
+    if (multiple) {
+      onChange(value.filter((id) => id !== ""));
+    } else {
+      onChange(value);
+    }
+  };
+
+  const handleCreatePrimerClick = () => {
+    setOpen(false);
+    goToPrimerTab();
+  };
+
   return (
     <FormControl fullWidth>
       <InputLabel>{label}</InputLabel>
       <Select
         multiple={multiple}
         value={selected}
-        onChange={(e) => onChange(e.target.value)}
+        onChange={(e) => onChangeHandleCreatePrimer(e.target.value)}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
         label={label}
         renderValue={multiple ? (s) => chipRenderer(s, primers) : undefined}
       >
-        <MenuItem onClick={goToPrimerTab} value="">
+        <MenuItem onClick={handleCreatePrimerClick} value="">
           <AddCircleIcon color="success" />
           <em style={{ marginLeft: 8 }}>Create primer</em>
         </MenuItem>
