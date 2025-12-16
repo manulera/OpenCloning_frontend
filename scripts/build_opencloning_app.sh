0c6c2fe013873b3e6f8d8398f2bd60648dc7b508
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-12-16 16:06:07 +0100

    compile in docker

--- scripts/build_opencloning_app.sh
+++ scripts/build_opencloning_app.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e  # exit immediately on error
 
 # Ensure the reset runs even if build fails
@@ -7,7 +7,7 @@ trap 'yarn workspace @opencloning/ui postpack' EXIT
 # Inject the real version into the library
 yarn workspace @opencloning/ui prepack
 
-# Build the app
-yarn workspace opencloning build
+# Build the app (pass through all arguments and flags)
+yarn workspace opencloning build "$@"
 
 # Reset happens automatically because of the trap
