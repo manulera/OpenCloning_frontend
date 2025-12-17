d8abc82472b0b611f9f629e0c3f40eb14ee8a175
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-12-17 16:20:41 +0100

    Start working on Assembler widget

---
+++ apps/syntax-builder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AssemblePartWidget } from '@opencloning/ui/components/assembler';
+
+function App() {
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <AssemblePartWidget />
+    </div>
+  );
+}
+
+export default App;
