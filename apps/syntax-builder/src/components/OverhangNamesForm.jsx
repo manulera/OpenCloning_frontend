8f79b93c292f3b85f3b39443bbb39c7a59ac0d92
Manuel Lera-Ramirez <manulera14@gmail.com>
2026-01-22 11:23:49 +0000

    use data grid instead

--- apps/syntax-builder/src/components/OverhangNamesForm.jsx
+++ apps/syntax-builder/src/components/OverhangNamesForm.jsx
@@ -1,39 +1,70 @@
 import React from 'react'
 import SectionWrapper from './SectionWrapper'
 import { useFormData } from '../context/FormDataContext';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
+import { DataGrid, useGridApiRef } from '@mui/x-data-grid';
 
 function OverhangNamesForm() {
   const { overhangNames, updateOverhangName } = useFormData();
-  const allOverhangs = React.useMemo(() => Object.keys(overhangNames), [overhangNames]);
+  const apiRef = useGridApiRef();
+  
+  // Transform overhangNames object into rows format for DataGrid
+  const rows = React.useMemo(() => 
+    Object.keys(overhangNames).map((overhang) => ({
+      id: overhang,
+      overhang,
+      name: overhangNames[overhang] || '',
+    })),
+  [overhangNames]);
+
+  const columns = React.useMemo(() => [
+    {
+      field: 'overhang',
+      headerName: 'Overhang',
+      width: 150,
+      editable: false,
+      renderCell: (params) => (
+        <span style={{ fontFamily: 'monospace' }}>{params.value}</span>
+      ),
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+      width: 300,
+      editable: true,
+    },
+  ], []);
+
+  const handleCellClick = React.useCallback((params) => {
+    if (params.isEditable && params.cellMode !== 'edit') {
+      apiRef.current.startCellEditMode({ id: params.id, field: params.field });
+    }
+  }, [apiRef]);
+
+  const processRowUpdate = React.useCallback((newRow) => {
+    updateOverhangName(newRow.id, newRow.name || '');
+    return newRow;
+  }, [updateOverhangName]);
+
   return (
     <SectionWrapper title="Overhang Names">
-      <TableContainer>
-        <Table size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ fontWeight: 'bold' }}>Overhang</TableCell>
-              <TableCell sx={{ fontWeight: 'bold' }}>Name</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {allOverhangs.map((overhang) => (
-              <TableRow key={overhang}>
-                <TableCell sx={{ fontFamily: 'monospace' }}>{overhang}</TableCell>
-                <TableCell>
-                  <TextField
-                    size="small"
-                    fullWidth
-                    value={overhangNames[overhang] || ''}
-                    onChange={(e) => updateOverhangName(overhang, e.target.value)}
-                    placeholder="Enter name"
-                  />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <DataGrid
+        apiRef={apiRef}
+        rows={rows}
+        columns={columns}
+        onCellClick={handleCellClick}
+        processRowUpdate={processRowUpdate}
+        density="compact"
+        disableRowSelectionOnClick
+        disableColumnSorting
+        disableColumnFilter
+        disableColumnMenu
+        hideFooter
+        autoHeight
+        sx={{
+          '& .MuiDataGrid-cell': { fontSize: '0.875rem' },
+          '& .MuiDataGrid-columnHeader': { fontWeight: 'bold' },
+        }}
+      />
     </SectionWrapper>
   )
 }
