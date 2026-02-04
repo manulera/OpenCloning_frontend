e79d66eb6a97ba2e61ef9b739f07d763908a8710
Manuel Lera-Ramirez <manulera14@gmail.com>
2026-02-04 14:49:31 +0000

    improve test coverage

---
+++ packages/ui/src/components/assembler/ExistingSyntaxDialog.cy.jsx
@@ -0,0 +1,194 @@
+import React from 'react';
+import ExistingSyntaxDialog from './ExistingSyntaxDialog';
+
+const mockSyntaxes = [
+  {
+    path: 'test-syntax-1',
+    name: 'Test Syntax 1',
+    description: 'First test syntax',
+  },
+  {
+    path: 'test-syntax-2',
+    name: 'Test Syntax 2',
+    description: 'Second test syntax',
+  },
+];
+
+const mockSyntaxData = {
+  name: 'Test Syntax',
+  parts: [],
+};
+
+const mockPlasmidsData = [
+  {
+    id: 1,
+    name: 'Test Plasmid',
+  },
+];
+
+describe('<ExistingSyntaxDialog />', () => {
+  beforeEach(() => {
+    // Intercept the index.json request
+    cy.intercept('GET', 'https://assets.opencloning.org/syntaxes/syntaxes/index.json', {
+      statusCode: 200,
+      body: mockSyntaxes,
+    }).as('getSyntaxes');
+  });
+
+  it('loads and displays syntaxes successfully', () => {
+    const onCloseSpy = cy.spy().as('onCloseSpy');
+    const onSyntaxSelectSpy = cy.spy().as('onSyntaxSelectSpy');
+
+    cy.mount(
+      <ExistingSyntaxDialog
+        onClose={onCloseSpy}
+        onSyntaxSelect={onSyntaxSelectSpy}
+      />,
+    );
+
+    cy.wait('@getSyntaxes');
+    cy.contains('Load an existing syntax').should('exist');
+    cy.contains('Test Syntax 1').should('exist');
+    cy.contains('First test syntax').should('exist');
+    cy.contains('Test Syntax 2').should('exist');
+    cy.contains('Second test syntax').should('exist');
+  });
+
+  it('successfully loads syntax and plasmids data when clicking a syntax', () => {
+    const onCloseSpy = cy.spy().as('onCloseSpy');
+    const onSyntaxSelectSpy = cy.spy().as('onSyntaxSelectSpy');
+
+    // Intercept syntax.json and plasmids.json requests
+    cy.intercept('GET', 'https://assets.opencloning.org/syntaxes/syntaxes/test-syntax-1/syntax.json', {
+      statusCode: 200,
+      body: mockSyntaxData,
+    }).as('getSyntaxData');
+
+    cy.intercept('GET', 'https://assets.opencloning.org/syntaxes/syntaxes/test-syntax-1/plasmids.json', {
+      statusCode: 200,
+      body: mockPlasmidsData,
+    }).as('getPlasmidsData');
+
+    cy.mount(
+      <ExistingSyntaxDialog
+        onClose={onCloseSpy}
+        onSyntaxSelect={onSyntaxSelectSpy}
+      />,
+    );
+
+    cy.wait('@getSyntaxes');
+    cy.contains('Test Syntax 1').click();
+
+    cy.wait('@getSyntaxData');
+    cy.wait('@getPlasmidsData');
+
+    cy.get('@onSyntaxSelectSpy').should('have.been.calledWith', mockSyntaxData, mockPlasmidsData);
+    cy.get('@onCloseSpy').should('have.been.called');
+  });
+
+  it('displays error message when syntax.json request fails', () => {
+    const onCloseSpy = cy.spy().as('onCloseSpy');
+    const onSyntaxSelectSpy = cy.spy().as('onSyntaxSelectSpy');
+
+    // Intercept syntax.json with error
+    cy.intercept('GET', 'https://assets.opencloning.org/syntaxes/syntaxes/test-syntax-1/syntax.json', {
+      statusCode: 500,
+      body: { error: 'Internal server error' },
+    }).as('getSyntaxDataError');
+
+    cy.mount(
+      <ExistingSyntaxDialog
+        onClose={onCloseSpy}
+        onSyntaxSelect={onSyntaxSelectSpy}
+      />,
+    );
+
+    cy.wait('@getSyntaxes');
+    cy.contains('Test Syntax 1').click();
+
+    cy.wait('@getSyntaxDataError');
+
+    cy.contains('Failed to load syntax data. Please try again.').should('exist');
+    cy.get('@onSyntaxSelectSpy').should('not.have.been.called');
+    cy.get('@onCloseSpy').should('not.have.been.called');
+  });
+
+  it('displays error message when plasmids.json request fails', () => {
+    const onCloseSpy = cy.spy().as('onCloseSpy');
+    const onSyntaxSelectSpy = cy.spy().as('onSyntaxSelectSpy');
+
+    // Intercept syntax.json successfully
+    cy.intercept('GET', 'https://assets.opencloning.org/syntaxes/syntaxes/test-syntax-1/syntax.json', {
+      statusCode: 200,
+      body: mockSyntaxData,
+    }).as('getSyntaxData');
+
+    // Intercept plasmids.json with error
+    cy.intercept('GET', 'https://assets.opencloning.org/syntaxes/syntaxes/test-syntax-1/plasmids.json', {
+      statusCode: 500,
+      body: { error: 'Internal server error' },
+    }).as('getPlasmidsDataError');
+
+    cy.mount(
+      <ExistingSyntaxDialog
+        onClose={onCloseSpy}
+        onSyntaxSelect={onSyntaxSelectSpy}
+      />,
+    );
+
+    cy.wait('@getSyntaxes');
+    cy.contains('Test Syntax 1').click();
+
+    cy.wait('@getSyntaxData');
+    cy.wait('@getPlasmidsDataError');
+
+    cy.contains('Failed to load plasmids data. Please try again.').should('exist');
+    cy.get('@onSyntaxSelectSpy').should('not.have.been.called');
+    cy.get('@onCloseSpy').should('not.have.been.called');
+  });
+
+  it('clears error message when selecting a different syntax after error', () => {
+    const onCloseSpy = cy.spy().as('onCloseSpy');
+    const onSyntaxSelectSpy = cy.spy().as('onSyntaxSelectSpy');
+
+    // First syntax fails
+    cy.intercept('GET', 'https://assets.opencloning.org/syntaxes/syntaxes/test-syntax-1/syntax.json', {
+      statusCode: 500,
+      body: { error: 'Internal server error' },
+    }).as('getSyntaxDataError');
+
+    // Second syntax succeeds
+    cy.intercept('GET', 'https://assets.opencloning.org/syntaxes/syntaxes/test-syntax-2/syntax.json', {
+      statusCode: 200,
+      body: mockSyntaxData,
+    }).as('getSyntaxData2');
+
+    cy.intercept('GET', 'https://assets.opencloning.org/syntaxes/syntaxes/test-syntax-2/plasmids.json', {
+      statusCode: 200,
+      body: mockPlasmidsData,
+    }).as('getPlasmidsData2');
+
+    cy.mount(
+      <ExistingSyntaxDialog
+        onClose={onCloseSpy}
+        onSyntaxSelect={onSyntaxSelectSpy}
+      />,
+    );
+
+    cy.wait('@getSyntaxes');
+    
+    // Click first syntax (fails)
+    cy.contains('Test Syntax 1').click();
+    cy.wait('@getSyntaxDataError');
+    cy.contains('Failed to load syntax data. Please try again.').should('exist');
+
+    // Click second syntax (succeeds) - error should be cleared
+    cy.contains('Test Syntax 2').click();
+    cy.wait('@getSyntaxData2');
+    cy.wait('@getPlasmidsData2');
+
+    cy.contains('Failed to load syntax data. Please try again.').should('not.exist');
+    cy.get('@onSyntaxSelectSpy').should('have.been.calledWith', mockSyntaxData, mockPlasmidsData);
+    cy.get('@onCloseSpy').should('have.been.called');
+  });
+});
