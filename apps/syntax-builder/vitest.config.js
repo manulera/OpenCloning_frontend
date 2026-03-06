d7c7a981519349fac69013f8e47edc7f1d12635f
Manuel Lera-Ramirez <manulera14@gmail.com>
2026-03-06 10:33:58 +0000

    fix vitest resolve

--- apps/syntax-builder/vitest.config.js
+++ apps/syntax-builder/vitest.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vitest/config';
-import { resolve } from 'path';
 import { testConfig } from '../../vitest.common.config';
 
 export default defineConfig({
   test: {
     ...testConfig,
-  },
-  resolve: {
-    alias: {
-      '@opencloning/ui': resolve(__dirname, '../../packages/ui/src'),
-      '@opencloning/store': resolve(__dirname, '../../packages/store/src'),
-      '@opencloning/utils': resolve(__dirname, '../../packages/utils/src/utils'),
-    },
   },
 });
 
