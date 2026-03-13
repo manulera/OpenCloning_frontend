65c5b4a5398260fc2b8541c83ccd95a945c39c37
Manuel Lera-Ramirez <manulera14@gmail.com>
2026-03-13 15:33:03 +0000

    make querySelect component

---
+++ apps/opencloningdb/src/components/AlleleSelect.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { QuerySelect, useDebouncedSearchQuery } from '@opencloning/ui';
+import { openCloningDBHttpClient, endpoints } from '@opencloning/opencloningdb';
+
+const getQuery = (name) => ({
+  queryKey: ['sequences', { sequence_types: ['allele'], name }],
+  queryFn: async () => {
+    const { data } = await openCloningDBHttpClient.get(endpoints.sequences, {
+      params: { sequence_types: ['allele'], name },
+    });
+    return data.items;
+  },
+});
+
+function AlleleSelect({ onChange, label = 'Alleles', multiple = true, ...rest }) {
+  const { query, autocompleteProps } = useDebouncedSearchQuery(getQuery);
+
+  return (
+    <QuerySelect
+      sx={{ minWidth: 240, margin: 4 }}
+      query={query}
+      label={label}
+      multiple={multiple}
+      getOptionLabel={(seq) => seq.name ?? `Sequence ${seq.id}`}
+      onChange={onChange}
+      autocompleteProps={autocompleteProps}
+      {...rest}
+    />
+  );
+}
+
+export default AlleleSelect;
