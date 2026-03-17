2978775961d13cbbfdca9d65ec1caf9f3c82ea8f
Manuel Lera-Ramirez <manulera14@gmail.com>
2026-03-17 16:02:43 +0000

    working on speeddial

--- packages/ui/src/components/CloningSpeedDial.jsx
+++ packages/ui/src/components/CloningSpeedDial.jsx
@@ -3,7 +3,7 @@ import { Box, SpeedDial, SpeedDialAction } from '@mui/material'
 import { Add as AddIcon, Build as BuildIcon, Search as SearchIcon, PushPin as PushPinIcon } from '@mui/icons-material'
 import useDatabase from '../hooks/useDatabase';
 import { batch, useDispatch, useStore } from 'react-redux';
-import { getSequenceIdsThatAreInput } from '@opencloning/utils/network';
+import { getSequenceIdsThatAreInput, getSequenceIdsThatAreNotInput } from '@opencloning/utils/network';
 import { cloningActions } from '@opencloning/store/cloning';
 
 const { deleteSourceAndItsChildren, addSourceAndItsOutputSequence } = cloningActions;
@@ -16,24 +16,37 @@ function useLocateSequencesInDatabaseAction() {
 
   const main = React.useCallback(async () => {
     const { sequences, sources } = store.getState().cloning;
-    const sequence = sequences[0];
-    const result = await database.locateSequenceInDatabase(sequence);
-    if (result.length) {
-      const sequenceIdsThatAreInput = getSequenceIdsThatAreInput(sources);
-      if (!sequenceIdsThatAreInput.includes(sequence.id)) {
-        const newSource = {
-          input: [],
-          type: 'DatabaseSource',
-          database_id: result[0].sequence_ref.id,
-        };
-        const newSequence = result[0].sequence
-        batch(() => {
-          dispatch(deleteSourceAndItsChildren(sequence.id));
-          dispatch(addSourceAndItsOutputSequence({ source: newSource, sequence: newSequence }));
-        });
-      }
-      console.log(result);
+
+    const promises = sequences.map((sequence) => database.locateSequenceInDatabase(sequence));
+    const results = await Promise.all(promises);
+    const resultsMap = new Map();
+    for (let i = 0; i < results.length; i++) {
+      resultsMap.set(sequences[i].id, results[i]);
     }
+    const handledSequenceIds = new Set();
+    const allSequenceIds = new Set(sequences.map((sequence) => sequence.id));
+
+    const sequenceIdsThatAreNotInput = getSequenceIdsThatAreNotInput(sequences, sources);
+    console.log(sequenceIdsThatAreNotInput);
+    let iter = 0;
+    // while (handledSequenceIds.size < sequences.length || iter < sequences.length) {
+    //   iter++;
+    //   const sequenceIdsThatAreInput = new Set(getSequenceIdsThatAreInput(sources));
+
+    //   if (!sequenceIdsThatAreInput.includes(sequence.id)) {
+    //     const newSource = {
+    //       input: [],
+    //       type: 'DatabaseSource',
+    //       database_id: result[0].sequence_ref.id,
+    //     };
+    //     const newSequence = result[0].sequence
+    //     batch(() => {
+    //       dispatch(deleteSourceAndItsChildren(sequence.id));
+    //       dispatch(addSourceAndItsOutputSequence({ source: newSource, sequence: newSequence }));
+    //     });
+    //   }
+    //   console.log(result);
+    // }
   }, [database, store, dispatch]);
   return main;
 }
