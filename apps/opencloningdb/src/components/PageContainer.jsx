da7141dd4996f37314c139b63f0d348e5062822d
Manuel Lera-Ramirez <manulera14@gmail.com>
2026-03-20 13:04:23 +0000

    add linked primers to sequences

--- apps/opencloningdb/src/components/PageContainer.jsx
+++ apps/opencloningdb/src/components/PageContainer.jsx
@@ -3,7 +3,7 @@ import { Box } from '@mui/material'
 
 function PageContainer({ children, ...rest }) {
   return (
-    <Box sx={{ flex: 1, minHeight: 0, overflowY: 'auto', p: 3 }} {...rest}>
+    <Box sx={{ flex: 1, minHeight: 0, overflowY: 'auto', p: 5 }} {...rest}>
       {children}
     </Box>
   )
