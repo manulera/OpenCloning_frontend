57cc4c063854bf6b25489996ced01bd82d8afb5d
Manuel Lera-Ramirez <manulera14@gmail.com>
2026-05-01 15:07:52 +0100

    add coverage to opencloningdb vite.config

--- apps/opencloningdb/vite.config.js
+++ apps/opencloningdb/vite.config.js
@@ -1,8 +1,37 @@
 import react from '@vitejs/plugin-react';
+import istanbul from 'vite-plugin-istanbul';
+import { resolve } from 'path';
 
 export default () => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    (process.env.VITE_COVERAGE) && istanbul({
+      include: [
+        'packages/*/src/**/*',
+        'apps/*/src/**/*'
+      ],
+      extension: ['.js', '.jsx'],
+      cwd: resolve(__dirname, '../..'),
+    }),
+  ],
   server: {
     port: 3002,
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-redux',
+      '@mui/material',
+
+    ],
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+  build: {
+    outDir: 'build',
   },
 });
